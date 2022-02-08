5f4ce96b86fa5b3478318763269df441fe248909
Prabhat Shaw <Prabhat.Shaw77@yahoo.com>
2022-02-08 13:40:08 +0530

    added cors

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,20 @@
 import express from 'express';
-// import cors from 'cors';
+import cors from 'cors';
 // import cookieParser from 'cookie-parser';
 import PassportAuthRoute from './passport';
 import AuthRoute from './raw-integration';
 
-const { PORT, AUTH_VIA_PASSPORT } = process.env;
+const { PORT, AUTH_VIA_PASSPORT, CLIENT_ROOT_URL } = process.env;
 const app = express();
 
-// app.use(
-//   cors({
-//     // Sets Access-Control-Allow-Origin to the UI URI
-//     origin: CLIENT_ROOT_URL,
-//     // Sets Access-Control-Allow-Credentials to true
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    // Sets Access-Control-Allow-Origin to the UI URI
+    origin: CLIENT_ROOT_URL,
+    // Sets Access-Control-Allow-Credentials to true
+    credentials: true,
+  })
+);
 
 // app.use(cookieParser());
 app.use('/api/auth', AUTH_VIA_PASSPORT === 'true' ? PassportAuthRoute : AuthRoute);
